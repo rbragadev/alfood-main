c288a2bc859d62ffc5a3c4b958621e015145854c
Raphael Braga <raffael.info@gmail.com>
2025-01-04 18:02:22 -0300

    feat/admin section restaurants

--- src/componentes/ListaRestaurantes/Prato/index.tsx
+++ src/componentes/ListaRestaurantes/Prato/index.tsx
@@ -1,42 +1,30 @@
-import { useEffect, useState } from 'react';
-import IPrato from '../../../interfaces/IPrato';
-import estilos from './Prato.module.scss';
-import axios from 'axios';
-import { IPaginacao } from '../../../interfaces/IPaginacao';
+import { useEffect, useState } from "react";
+import IPrato from "../../../interfaces/IPrato";
+import estilos from "./Prato.module.scss";
+import axios from "axios";
+import { IPaginacao } from "../../../interfaces/IPaginacao";
 
 interface PratoProps {
-  prato: IPrato
+  prato: IPrato;
 }
 
 const Prato = ({ prato }: PratoProps) => {
-
-  const [pratos, setPratos] = useState<IPrato[]>([])
-
-  useEffect(() => {
-    axios.get<IPaginacao<IPrato>>("http://localhost:8000/api/v1/pratos/")
-      .then((res) => {
-        setPratos(res.data.results)
-      })
-  })
-
-  return (<div className={estilos.Prato}>
-    <div className={estilos.Container}>
-      <div>
-        <div className={estilos.EfeitoTorcao}>
-          <img src={prato.imagem} alt={prato.descricao}/>
+  return (
+    <div className={estilos.Prato}>
+      <div className={estilos.Container}>
+        <div>
+          <div className={estilos.EfeitoTorcao}>
+            <img src={prato.imagem} alt={prato.descricao} />
+          </div>
         </div>
       </div>
-    </div>
-    <div className={estilos.Conteudo}>
-      <h3>{prato.nome}</h3>
-      <div className={estilos.Tag}>
-        {prato.tag}
-      </div>
-      <div>
-        {prato.descricao}
+      <div className={estilos.Conteudo}>
+        <h3>{prato.nome}</h3>
+        <div className={estilos.Tag}>{prato.tag}</div>
+        <div>{prato.descricao}</div>
       </div>
     </div>
-  </div>)
-}
+  );
+};
 
-export default Prato
+export default Prato;
--- src/componentes/NavBar/index.tsx
+++ src/componentes/NavBar/index.tsx
@@ -1,17 +1,22 @@
-import estilos from './NavBar.module.scss';
-import { Link } from 'react-router-dom'
+import estilos from "./NavBar.module.scss";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (<nav className={estilos.Link}>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/restaurantes">Restaurantes</Link>
-      </li>
-    </ul>
-  </nav>)
-}
+  return (
+    <nav className={estilos.Link}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/restaurantes">Restaurantes</Link>
+        </li>
+        <li>
+          <Link to="/admin/restaurantes">Admin</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
